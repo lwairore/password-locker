dd3036b950232090fdfe3cd17bfe7a4b0dccd8e3
lwairore <kwairore@gmail.com>
2019-05-11 21:23:46 +0300

    Defiine setUp() method that instantiates credentials_test instance before any tests can be run

--- test_credentials.py
+++ test_credentials.py
@@ -1,7 +1,19 @@
 import unittest
+from credentials import Credentials
+
 
 class TestCredentials(unittest.TestCase):
+    
     """
     Define a TestCredentials subclass that inherits properties and behaviours from
-    unittest.TestCase superclass
+    unittest.TestCase superclass above.
+    Below, define a setUp() method that instantiates new_credentials
+    instance of class Credentials before any tests can be run
     """
+
+    def setUp(self):
+        self.creadentials_test = Credentials("lwairore", "Yahoo", "4280")
+
+
+
+    
