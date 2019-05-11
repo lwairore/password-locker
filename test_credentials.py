76385c8e913cc46852c0139bc0b141cd17c2fd43
lwairore <kwairore@gmail.com>
2019-05-11 21:43:33 +0300

    Defined test_save_individual_credential() method that takes in an instance. Called save_credential() method that takes in credentials instance, use assertEqual() method to assertain the lenght of Credentials class variable's length is equal to one after calling save_credentials() method

--- test_credentials.py
+++ test_credentials.py
@@ -21,4 +21,23 @@ class TestCredentials(unittest.TestCase):
         """
         Credentials.credentials = []
 
-    
+    def test_save_individual_credential(self):
+        """
+        This method will take take in an instance that 
+        it will later on use that instance to call save_credential() method
+        that will also pass in that instance as self.Finally, these method
+        will use assertEqual to assertain if the length of credentials class variable is
+        equal to one(1)        
+        """    
+        self.creadentials_test.save_credential()
+        self.assertEqual(len(Credentials.credentials), 1)
+
+
+
+
+if __name__ == "__main__":
+    """
+    Checks if this file is being run as the main file
+    or has been imported.
+    """
+    unittest.main()
