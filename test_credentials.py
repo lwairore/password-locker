804f07b9d1977a4bb4b3b934610471671e5cb009
lwairore <kwairore@gmail.com>
2019-05-11 21:17:53 +0300

    Define a TestCredentials subclass that inherits properties and behaviours from unittest.TestCase superclass

--- test_credentials.py
+++ test_credentials.py
@@ -1,3 +1,7 @@
 import unittest
 
-
+class TestCredentials(unittest.TestCase):
+    """
+    Define a TestCredentials subclass that inherits properties and behaviours from
+    unittest.TestCase superclass
+    """
