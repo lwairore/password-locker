638000b1fb6a0218adebb2c240e84c72566a9b62
lwairore <kwairore@gmail.com>
2019-05-11 21:27:55 +0300

    Create an empty class variable credentials = []

--- credentials.py
+++ credentials.py
@@ -7,8 +7,10 @@ class Credentials:
     will have: username, account_name, password upon 
     instantiation.
     """
+    credentials = []
     def __init__(self, user_name, account_name, password):
         self.user_name = user_name
         self.account_name = account_name
         self.password = password
-        
+
+    
