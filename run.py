7d051dc87a648a24ea901be5c5cbe9c962131605
lwairore <kwairore@gmail.com>
2019-05-12 09:14:28 +0300

    Define create_new_user() function that takes two arguments, name and login_password. Instantiates a new instance new_user of class User based on user name and login_password input. Returns new_user instance when called.

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ standalone executable without typing python3.6
 beforehand in the terminal.
 """
 from credentials import Credentials
+from user import User
 
 
 def create_new_credential(user_name, account_name, passsword):
@@ -13,3 +14,13 @@ def create_new_credential(user_name, account_name, passsword):
     """
     new_credential = Credentials(user_name, account_name, password)
     return new_credential
+
+
+def create_new_user(name, login_password):
+    """
+    This function takes two arguments based on the user inputs;
+    name and login_password. 
+    Returns new_user instance when called.
+    """
+    new_user = User(name, login_password)
+    return new_user
