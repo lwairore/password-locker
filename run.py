906131267d507e26b8e696d1ae7ddb34e8c6b45a
lwairore <kwairore@gmail.com>
2019-05-12 09:02:12 +0300

    Add shebanng that points out to the python3.6 interpreter locale

---
+++ run.py
@@ -0,0 +1,8 @@
+#!usr/bin/env python3.6
+"""
+#! shebang line determines the script's ability to be executed like an
+standalone executable without typing python3.6
+beforehand in the terminal.
+"""
+
+
