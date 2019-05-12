6692124a21548d8826cb79b0b61f65c4064588c7
lwairore <kwairore@gmail.com>
2019-05-12 08:54:32 +0300

    Define test_init() method that assertanins whether the name and login_password key values are equal to expected values

--- test_user.py
+++ test_user.py
@@ -19,3 +19,19 @@ class TestUser(unittest.TestCase):
         This method empties users class variable of class User.
         """
         User.users = []
+
+    def test_init(self):
+        """
+        This method does the actually asserting by checking for equality
+        between the value of name and login_password keys with the expeted values.
+        """
+        self.assertEqual(self.new_user.name, "john")
+        self.assertEqual(self.new_user.login_password, "8754lope")
+
+
+if __name__ == "__main__":
+    """
+    Checks if where this module is ran. Executes the module if it's 
+    run as main.
+    """
+    unittest.main()
